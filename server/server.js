76e4a57e5b1b1e6828fc4691fd64c35ad8727813
Rosa Dolson <rosa.dolson@gmail.com>
2017-12-05 19:15:10 -0700

    Reverted Code Base

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-const Post = require('./models/Post')
 
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
@@ -12,7 +11,6 @@ mongoose.connect('mongodb://localhost/MINI-INSTAGRAM_DB')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
-
 
 const server = app.listen(port, () => console.log(`Running on port: ${port} 🔥`))
 
